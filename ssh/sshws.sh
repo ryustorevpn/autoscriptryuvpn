abed7f949ca36947d13996ef3a325c5427dd0eb6
ryustorevpn <111565474+ryustorevpn@users.noreply.github.com>
2022-08-19 22:13:30 +0700

    Update sshws.sh

--- ssh/sshws.sh
+++ ssh/sshws.sh
@@ -14,7 +14,7 @@ portsshws=`cat ~/log-install.txt | grep -w "SSH Websocket" | cut -d: -f2 | awk '
 if [ -f "/etc/systemd/system/sshws.service" ]; then
 clear
 else
-wget -q -O /usr/bin/proxy3.js "https://autosscript.site/aio/ssh/proxy3.js"
+wget -q -O /usr/bin/proxy3.js "https://github.com/ryustorevpn/autoscriptryuvpn/main/ssh/proxy3.js"
 cat <<EOF> /etc/systemd/system/sshws.service
 [Unit]
 Description=WSenabler
@@ -41,7 +41,7 @@ PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
 if [[ ! -z "${PID}" ]]; then
 echo "Already ON !"
 else
-wget -q -O /usr/bin/ssh-wsenabler "https://autosscript.site/aio/ssh/sshws-true.sh" && chmod +x /usr/bin/ssh-wsenabler && /usr/bin/ssh-wsenabler
+wget -q -O /usr/bin/ssh-wsenabler "https://github.com/ryustorevpn/autoscriptryuvpn/main/ssh/sshws-true.sh" && chmod +x /usr/bin/ssh-wsenabler && /usr/bin/ssh-wsenabler
 systemctl daemon-reload >/dev/null 2>&1
 systemctl enable sshws.service >/dev/null 2>&1
 systemctl start sshws.service >/dev/null 2>&1
