20e317db863003a482cebaf238bda39018d6f4f0
ryustorevpn <111565474+ryustorevpn@users.noreply.github.com>
2022-08-19 23:13:08 +0700

    Update insshws.sh

--- install/insshws.sh
+++ install/insshws.sh
@@ -5,8 +5,8 @@ cd
 
 #Install Script Websocket-SSH Python
 #wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/sallxd/sl/main/sshws/openssh-socket.py
-wget -O /usr/local/bin/ws-dropbear https://autosscript.site/aio/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://autosscript.site/aio/ws-stunnel
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/ryustorevpn/autoscriptryuvpn/main/ws/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/ryustorevpn/autoscriptryuvpn/main/ws/ws-stunnel
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
@@ -20,10 +20,10 @@ chmod +x /usr/local/bin/ws-stunnel
 #wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sallxd/sl/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://autosscript.site/aio/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/ryustorevpn/autoscriptryuvpn/main/ws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://autosscript.site/aio/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/ryustorevpn/autoscriptryuvpn/main/ws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 ##System Websocket-OpenVPN Python
 #wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
