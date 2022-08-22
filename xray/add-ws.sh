11bcd9ad93bd1d12e7fa06bb89bca59d7fc072b0
ryustorevpn <111565474+ryustorevpn@users.noreply.github.com>
2022-08-23 03:46:11 +0700

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -103,7 +103,7 @@ systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\\E[0;41;36m======= Informasi Account Xray/Vmess =======\E[0m" | tee -a /etc/log-create-user.log
+echo -e "\\E[0;41;36m======= INFORMASI AKUN VMESS =======\E[0m" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Domain : ${domain}" | tee -a /etc/log-create-user.log
