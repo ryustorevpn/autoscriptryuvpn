44f938193580d25f6cb8ab18682f73cb8cbd8296
ryustorevpn <111565474+ryustorevpn@users.noreply.github.com>
2022-08-23 03:51:29 +0700

    Update cek-ws.sh

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -14,7 +14,7 @@ echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
 
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[0;41;36m         XRAY User Login          \E[0m"
+echo -e "\\E[0;41;36m========== INFORMASI USER LOGIN XRAY ==========\E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 for akun in "${data[@]}"
